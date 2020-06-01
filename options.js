d9294326caec196d05d30517fc5c7534bec3f2b7
Imaculate Mosha <imaculatemosha@yahoo.com>
2020-05-31 17:18:38 -0700

    Setting options correctly

--- options.js
+++ options.js
@@ -1,7 +1,7 @@
 function setVolume()
 {
     let vlm = document.getElementById('volumePicker').volume;
-    chrome.storage.sync.set({volume: vlm}, function() {
+    chrome.storage.sync.set({'volume': vlm}, function() {
         console.log('Volume is set to' + vlm);
         alert("Got it! Volume saved to "+ vlm);
     });
