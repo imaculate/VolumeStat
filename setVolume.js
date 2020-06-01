9939c29b00c6f202944ef964b1e77ed11f9f79db
Imaculate Mosha <imaculatemosha@yahoo.com>
2020-06-01 09:52:05 -0700

    Finally working version! �

--- setVolume.js
+++ setVolume.js
@@ -2,8 +2,8 @@ chrome.storage.sync.get(['volume'], function(data) {
     var videos = document.getElementsByTagName("video");
     if (videos.length > 0)
     {
-        console.log('Found '+ videos.length + ' videos');
-        console.log('Setting volume to: '+ data.volume);
+        console.log('VolumeStat: Found '+ videos.length + ' videos');
+        console.log('VolumeStat: Setting volume to: '+ data.volume);
         var i;
         for (i = 0; i < videos.length; i++) {
             videos[i].volume = data.volume;
@@ -13,8 +13,8 @@ chrome.storage.sync.get(['volume'], function(data) {
     var audios = document.getElementsByTagName("audio");
     if (audios.length > 0)
     {
-        console.log('Found '+ audios.length + ' audios');
-        console.log('Setting volume to: '+ data.volume);
+        console.log('VolumeStat: Found '+ audios.length + ' audios');
+        console.log('VolumeStat: Setting volume to: '+ data.volume);
         var j;
         for (j = 0; j < audios.length; j++) {
             audios[j].volume = data.volume;
